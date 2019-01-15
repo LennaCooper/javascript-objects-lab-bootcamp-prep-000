8cff19c6cb3035f23c7fd1f7a0c207c6540a7089
Lenna Cooper <lenna.cooper@gmail.com>
2019-01-15 20:27:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value
- 
-  return (object)
-}
-  
-var recipes = { 
+ var recipes = { 
  prop: 1,
  prop2: 2
 }  
+  return (object)
+}
+  
+
   
 
